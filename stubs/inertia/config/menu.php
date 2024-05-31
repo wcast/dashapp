7073f15cbef57aecc6be409a35f9001295b9f55b
Rogério Lopes <rogerio.wcast@gmail.com>
2024-05-31 13:45:27 -0300

    Ajustes

--- stubs/inertia/config/menu.php
+++ stubs/inertia/config/menu.php
@@ -62,51 +62,5 @@ return [
                 ]
             ]
         ],
-        [
-            'title' => 'Condomínio',
-            'description' => 'Lista de condomínios',
-            'manual' => '/',
-            'segment' => null,
-            'icon' => 'mdi mdi-office-building-marker',
-            'route' => 'condominium',
-            'url' => null,
-            'info' => null,
-            'info_text' => null,
-            'display_menu' => 'S',
-            'order' => 2,
-            'status' => 'A',
-            'sub_menus' => [
-                [
-                    'title' => 'Listar condomínios',
-                    'description' => 'Listar condomínios',
-                    'manual' => '/',
-                    'segment' => null,
-                    'icon' => 'mdi mdi-view-list',
-                    'route' => 'condominium',
-                    'url' => null,
-                    'info' => null,
-                    'info_text' => null,
-                    'display_menu' => 'S',
-                    'order' => 1,
-                    'status' => 'A',
-                    'sub_menus' => []
-                ],
-                [
-                    'title' => 'Novo condomínio',
-                    'description' => 'Novo condomínio',
-                    'manual' => '/',
-                    'segment' => null,
-                    'icon' => 'mdi mdi-plus',
-                    'route' => 'condominium.add',
-                    'url' => null,
-                    'info' => null,
-                    'info_text' => null,
-                    'display_menu' => 'S',
-                    'order' => 1,
-                    'status' => 'A',
-                    'sub_menus' => []
-                ]
-            ]
-        ]
     ]
 ];
