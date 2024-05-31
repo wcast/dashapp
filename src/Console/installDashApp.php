473e6be3f0ee6f3651a16127071a88492c5f28e6
Rogério Lopes <rogerio.wcast@gmail.com>
2024-05-30 21:57:15 -0300

    Ajustes

--- src/Console/installDashApp.php
+++ src/Console/installDashApp.php
@@ -77,7 +77,7 @@ trait installDashApp
         // Views...
         copy(__DIR__ . '/../../stubs/inertia/resources/views/app.blade.php', resource_path('views/app.blade.php'));
         copy(__DIR__ . '/../../stubs/inertia/resources/views/home.blade.php', resource_path('views/home.blade.php'));
-        copy(__DIR__ . '/../../stubs/inertia/resources/views/layout/pagination/default.blade.php', resource_path('views/layout/pagination/default.blade.php'));
+        copy(__DIR__ . '/../../stubs/inertia/resources/views/layout/pagination.blade.php', resource_path('views/layout/pagination.blade.php'));
         copy(__DIR__ . '/../../stubs/inertia/resources/views/layout/default.blade.php', resource_path('views/layout/default.blade.php'));
         copy(__DIR__ . '/../../stubs/inertia/resources/views/layout/footer.blade.php', resource_path('views/layout/footer.blade.php'));
         copy(__DIR__ . '/../../stubs/inertia/resources/views/layout/head.blade.php', resource_path('views/layout/head.blade.php'));
