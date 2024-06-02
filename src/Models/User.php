30b5f624970aae1e0d3613845b0083a24bb7f1a5
Rogério Lopes <rogerio.wcast@gmail.com>
2024-06-02 17:45:36 -0300

    Ajustes

--- src/Models/User.php
+++ src/Models/User.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use App\Models\Dashapp\Perfil;
+use App\Models\Perfil;
 use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
